e4fd1288680919ec5a15311943c67eb8e510f5af
Ebrahimuhsin <muhsinkm1002@gmail.com>
2024-08-13 13:34:16 +0530

    hello

---
+++ arithmetic_opp.py
@@ -0,0 +1,49 @@
+# print(10+10)
+# print('Ebrahim' + 'muhsin')
+# print(10-5.9)
+
+
+# wont work
+# print('ebrahim'-'a')
+
+# print (10*10)
+# print('ebrahim '*4)
+
+# print(10%3)
+# print(11%3)
+
+
+# print(10**2)
+
+# print(10//2)
+
+#  assignemnt operators( '=','*=','+=','-=','/=','%=','//=','**=' )
+# a=10
+# print(a)
+
+
+# a=10
+# a+=4
+# print(a)
+
+# a=11
+# a-=2
+# print(a)
+
+# a=10
+# a/=2
+# print(a)
+
+# a=10
+# a%=3+
+# print(a)
+
+
+# a=10
+# a**=2
+# print(a)
+
+
+# a=10
+# a//=2
+# print(a)
