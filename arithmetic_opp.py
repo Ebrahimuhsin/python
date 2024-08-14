3cd083e69d07f81fd73c992ea75c6ce75ca44275
Ebrahimuhsin <muhsinkm1002@gmail.com>
2024-08-14 13:29:36 +0530

    hello

--- arithmetic_opp.py
+++ arithmetic_opp.py
@@ -47,3 +47,77 @@
 # a=10
 # a//=2
 # print(a)
+
+
+
+                                                         #    logical operators(and,or,not)
+# print(10==10 and 10>=11)
+        # True      True      =True
+        # False       False     =False
+        # False         True      =false
+        # True            False     =False
+
+# print(10==11 or 10>=9)
+#         True    True    =True
+        # False   True    =True
+        # True    False   =True
+        # false   False   =False
+
+# print(not(10==10))
+    # if statement is true by using not it gives the output in opposite
+                                                        # MEMBERSHIP OPERATOR
+# a='python'
+# l='z'
+
+# print(l in a)
+
+                # using not in
+# a='python'
+# l='p'
+
+# print(l not in a)
+
+
+                                                        # identify operator (is ,isnot)
+# a=10
+# m=11
+# print(id(a))
+# print(id(m))
+# print(a is m)
+
+# a=10
+# m=10
+# print(id(a))
+# print(id(m))
+# print(a is not m)
+
+
+# a=10
+# m=10
+# print(id(a))
+# print(a is m)
+
+# a='m'
+# a1='u'
+# a2='h'
+# a3='s'
+# a4='i'
+# a5='n'
+# print(id(a))
+# print(id(a1))
+# print(id(a2))
+# print(id(a3))
+# print(id(a4))
+# print(id(a5))
+
+# print(a is [a1,a2,a3,a4,a5])
+
+
+
+
+
+#                                                                 STRING
+SET OF CHAREACTRES
+
+    
+
